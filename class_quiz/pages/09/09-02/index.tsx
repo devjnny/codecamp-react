328cd1cd7dd32e472b2ab475aeeca5ffbf2fc651
devjnny <suuujj@gmail.com>
2024-04-22 17:11:27 +0900

    ✏️react study: section18-페이지네이션_퀴즈

---
+++ class_quiz/pages/09/09-02/index.tsx
@@ -0,0 +1,107 @@
+import {
+	IQuery,
+	IQueryFetchBoardsArgs,
+	IQueryFetchBoardsCountArgs
+} from '@/src/commons/types/generated/types';
+import { useQuery, gql } from '@apollo/client';
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+
+const FETCH_BOARDS = gql`
+	query fetchBoards($page: Int) {
+		fetchBoards(page: $page) {
+			_id
+			writer
+			title
+			contents
+		}
+	}
+`;
+
+const FETCH_BOARDS_COUNT = gql`
+	query {
+		fetchBoardsCount
+	}
+`;
+
+const Pagination = styled.span`
+	color: ${(props: { isActive: boolean }) => (props.isActive ? 'red' : 'black')};
+`;
+
+const PageMoveButton = styled.span`
+	color: ${(props: { disabled: boolean }) => (props.disabled ? 'gray' : 'black')};
+`;
+
+export const Quiz09_02 = () => {
+	//09-01 quiz + 페이징 조건부 렌더링 -> filter로 변경
+	const [startPage, setStartPage] = useState(1330);
+	const [currentPage, setCurrentPage] = useState(1);
+	const { data, refetch } = useQuery<Pick<IQuery, 'fetchBoards'>, IQueryFetchBoardsArgs>(
+		FETCH_BOARDS,
+		{
+			variables: { page: startPage }
+		}
+	);
+	const { data: boardCount } = useQuery<
+		Pick<IQuery, 'fetchBoardsCount'>,
+		IQueryFetchBoardsCountArgs
+	>(FETCH_BOARDS_COUNT);
+
+	const lastPage = Math.ceil((boardCount?.fetchBoardsCount ?? 10) / 10);
+
+	const onClickPage = (event: React.MouseEvent<HTMLButtonElement>) => {
+		refetch({ page: Number(event.currentTarget.id) });
+		setCurrentPage(Number(event.currentTarget.id));
+	};
+
+	useEffect(() => {
+		setCurrentPage(startPage);
+	}, [startPage]); // 좋은 방법인지..
+
+	const onClickPrevPage = () => {
+		if (startPage === 1) return;
+		setStartPage((prev) => prev - 10);
+	};
+
+	const onClickNextPage = () => {
+		if (startPage + 10 >= lastPage) return;
+		setStartPage((prev) => prev + 10);
+	};
+
+	return (
+		<div>
+			{data?.fetchBoards.map((board) => (
+				<div key={board._id}>
+					<button style={{ margin: '10px' }}>{board.title}</button>
+					<span style={{ margin: '10px' }}>{board.writer}</span>
+				</div>
+			))}
+
+			<div>
+				<PageMoveButton disabled={startPage === 1} onClick={onClickPrevPage}>
+					이전페이지
+				</PageMoveButton>
+				{new Array(10)
+					.fill(1)
+					.filter((_, index) => {
+						return startPage + index <= lastPage;
+					})
+					.map((_, index) => (
+						<Pagination
+							isActive={currentPage === index + startPage}
+							key={index + startPage}
+							id={String(index + startPage)}
+							onClick={onClickPage}
+						>
+							{index + startPage}
+						</Pagination>
+					))}
+				<PageMoveButton disabled={startPage + 10 >= lastPage} onClick={onClickNextPage}>
+					다음페이지
+				</PageMoveButton>
+			</div>
+		</div>
+	);
+};
+
+export default Quiz09_02;
--- class_quiz/pages/_app.tsx
+++ class_quiz/pages/_app.tsx
@@ -1,26 +1,26 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import Layout from "../src/commons/layout";
-import { Global } from "@emotion/react";
-import globalStyles from "@/src/commons/styles/globalStyles";
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Layout from '../src/commons/layout';
+import { Global } from '@emotion/react';
+import globalStyles from '@/src/commons/styles/globalStyles';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: "http://backend-example.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(),
-  });
+	const client = new ApolloClient({
+		uri: 'http://backend-practice.codebootcamp.co.kr/graphql',
+		cache: new InMemoryCache()
+	});
 
-  return (
-    <div>
-      <ApolloProvider client={client}>
-        <>
-          <Global styles={globalStyles} />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </>
-      </ApolloProvider>
-    </div>
-  );
+	return (
+		<div>
+			<ApolloProvider client={client}>
+				<>
+					<Global styles={globalStyles} />
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</>
+			</ApolloProvider>
+		</div>
+	);
 }
